8551c3b0b167d9bb1ca247d1dafd9e2d5d2724ec
Jrod7938 <jrod7938@gmail.com>
2022-01-06 20:59:51 -0500

    Starter Code

--- Learning Openai Gym/setup.py
+++ Learning Openai Gym/setup.py
@@ -4,15 +4,28 @@ import random
 env = gym.make("CartPole-v1")
 
 def Random_games():
-    env.reset()
+    # Each of these episodes are their own game
+    for episode in range(10):
+        env.reset()
+        # This is each frame, up to 500... but we wont make it that far with random.
+        for t in range(500):
+            # This will display the environment
+            # Only display if ypu really want to see it.
+            # Takes much longer to display it.
+            env.render()
 
-    for t in range(500):
-        env.render()
+            # This will just create a simple action in any envirnment.
+            # In this environment, the action can be 0 or 1, which is left or right
+            action = env.action_space.sample()
 
-        action = env.action_space.sample()
+            # This executes the environment with an action
+            # and returns the observation of the environment,
+            # the reward, if the env is over, and other info.
+            next_state, reward, done, info = env.step(action)
 
-        next_state, reward, done, info = env.step(action)
-        if done:
-            break
+            # lets print everything in one line:
+            print(t, next_state, reward, done, info = env.step(action))
+            if done:
+                break
 
 Random_games()
